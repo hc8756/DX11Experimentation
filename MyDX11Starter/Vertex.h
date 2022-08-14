d7b2f35b6ccd7d80f5bee10c5669a2620acee6b8
Hyunseo Cho <hc8756@rit.edu>
2022-08-14 10:14:23 +0900

    refactoring

--- MyDX11Starter/Vertex.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <DirectXMath.h>
-
-//Definition of vertex data as it will be created in CPU-side for initial data
-struct Vertex
-{
-	DirectX::XMFLOAT3 Position;	    
-	DirectX::XMFLOAT4 Color;        
-};
